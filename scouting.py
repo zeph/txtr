74b97a96b8ecac57e977b3dc437e066ae597708b
Guido <zeph@fsfe.org>
2012-05-06 23:52:35 +0200

    a bit of multithreading

--- scouting.py
+++ scouting.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 __author__ = 'zeph'
-import jsonrpclib, json, pymongo, sys,  time
+import jsonrpclib, json, pymongo, sys,  time,  threading
+maxthreads = 3
 
 jsonrpclib.config.version = 1.0
 reaktor = jsonrpclib.Server('http://staging.txtr.com/json/rpc')
@@ -12,22 +13,31 @@ txtr_it = mongodb["txtr.it"]
 # http://www.hacksparrow.com/the-mongodb-tutorial.html
 txtr_it.remove() # "truncating" the collection
 
-def scout_catalog(category_ids = [], spacer = "\t"):
-    start_time = time.time()
-    #print "\nD: %s" % category_ids
-    if len(category_ids) == 0:
-        results = reaktor.WSContentCategoryMgmt.getCatalogContentCategoryRoots(token)
-        #print json.dumps(results, sort_keys=True, indent=4)
-        scout_catalog([ el["ID"] for el in results ])
-    print "\n"+spacer, 
-    for c_id in category_ids: 
-        print c_id, 
-        sys.stdout.flush() 
-        results = reaktor.WSContentCategoryMgmt.getContentCategory(token, c_id, False, None, False, 0, -1)
-        txtr_it.insert(results)
-        if len(results["childrenIDs"])!=0:
-            scout_catalog(results["childrenIDs"],  spacer+"\t")
-    print "\n%s(%s secs)" % (spacer[:-1], round(time.time() - start_time,  2)),    
+class ScoutCatalog(threading.Thread):
+    def __init__(self,  category_ids = []):
+        print ".", 
+        sys.stdout.flush()
+        threading.Thread.__init__(self)
+        self.category_ids = category_ids
+    def run(self):
+        pool_thr.acquire()
+        start_time = time.time()
+        if len(self.category_ids) == 0:
+            results = reaktor.WSContentCategoryMgmt.getCatalogContentCategoryRoots(token)
+            #print json.dumps(results, sort_keys=True, indent=4)
+            thr = ScoutCatalog([ el["ID"] for el in results ])
+            thr.start()
+        for c_id in self.category_ids: 
+            results = reaktor.WSContentCategoryMgmt.getContentCategory(token, c_id, False, None, False, 0, -1)
+            txtr_it.insert(results)
+            if len(results["childrenIDs"])!=0:
+                thr = ScoutCatalog(results["childrenIDs"])
+                thr.start()
+        pool_thr.release()
+        print "\n\t%s secs\t%s " % (round(time.time() - start_time,  2),  [str(x) for x in self.category_ids]), 
         
-scout_catalog() 
+pool_thr = threading.BoundedSemaphore(value=maxthreads)
+thr = ScoutCatalog() 
+thr.start()
+thr.join()
 print
