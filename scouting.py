833668957ce1cc55d627110d3fa38ee7b7c4c275
Guido <zeph@fsfe.org>
2012-05-06 19:50:13 +0200

    campa cavallo che l'erba cresce

--- scouting.py
+++ scouting.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
 __author__ = 'zeph'
-import jsonrpclib, json, pymongo
+import jsonrpclib, json, pymongo, sys
 
 jsonrpclib.config.version = 1.0
 reaktor = jsonrpclib.Server('http://staging.txtr.com/json/rpc')
-
-results = reaktor.WSAuth.authenticateAnonymousUser("txtr.it")
-token = results["token"]
-print token
+token="txtr.it"
 
 mongo_instance = pymongo.Connection('localhost', 27017)
 mongodb = mongo_instance.test_reaktor_stg
@@ -15,22 +12,20 @@ txtr_it = mongodb["txtr.it"]
 # http://www.hacksparrow.com/the-mongodb-tutorial.html
 txtr_it.remove() # "truncating" the collection
 
-results = reaktor.WSContentCategoryMgmt.getCatalogContentCategoryRoots(token)
-#print json.dumps(results, sort_keys=True, indent=4)
-category_ids = [ el["ID"] for el in results ]
-
-#txtr_it.insert(results)
-# commented out... 
-# since this datastructure is already a subset of what I'm gathering afterwards
-
-for c_id in category_ids:
-    print c_id
-    subresults = reaktor.WSContentCategoryMgmt.getContentCategory(token, c_id, False, None, False, 0, -1)
-    txtr_it.insert(subresults)
-    #print json.dumps(subresults, sort_keys=True, indent=4)
-    #print ("CAT", subresults["childrenIDs"])
-    #print subresults["documentIDs"]
-
-#for cat in txtr_it.find():
-#    print cat
-
+def scout_catalog(category_ids = [], spacer = " "):
+    #print "\nD: %s" % category_ids
+    if len(category_ids) == 0:
+        results = reaktor.WSContentCategoryMgmt.getCatalogContentCategoryRoots(token)
+        #print json.dumps(results, sort_keys=True, indent=4)
+        scout_catalog([ el["ID"] for el in results ])
+    print "\n"+spacer, 
+    for c_id in category_ids: 
+        print c_id, 
+        sys.stdout.flush() 
+        results = reaktor.WSContentCategoryMgmt.getContentCategory(token, c_id, False, None, False, 0, -1)
+        txtr_it.insert(results)
+        if len(results["childrenIDs"])!=0:
+            scout_catalog(results["childrenIDs"],  spacer+" ")
+        
+scout_catalog() 
+print
